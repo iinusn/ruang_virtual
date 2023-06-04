6461855a78ddcc28bd321e80769259d1fea4f178
iinusn <135322012+iinusn@users.noreply.github.com>
2023-06-04 12:39:20 +0800

    Update urls.py

--- projekku/ecomm/toko/urls.py
+++ projekku/ecomm/toko/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from . import views
 
+from . import views
+from .views import WaListView
+from .views import RiListView
+from .views import NeListView
 app_name = 'toko'
 
 urlpatterns = [
      path('', views.HomeListView.as_view(), name='home-produk-list'),
+     
      path('product/<slug>/', views.ProductDetailView.as_view(), name='produk-detail'),
      path('checkout/', views.CheckoutView.as_view(), name='checkout'),
      path('add-to-cart/<slug>/', views.add_to_cart, name='add-to-cart'),
@@ -13,4 +17,8 @@ urlpatterns = [
      path('payment/<payment_method>', views.PaymentView.as_view(), name='payment'),
      path('paypal-return/', views.paypal_return, name='paypal-return'),
      path('paypal-cancel/', views.paypal_cancel, name='paypal-cancel'),
+     path('watch/', views.WaListView.as_view(),{'kategori':'WA'},name='watch'),
+     path('ring/', views.RiListView.as_view(),{'kategori':'RI'},name='ring/'),
+     path('necklace/', views.NeListView.as_view(),{'kategori':'RI'},name='necklace/'),
+     path('earing/', views.NeListView.as_view(),{'kategori':'ER'},name='earing/'),
 ]
